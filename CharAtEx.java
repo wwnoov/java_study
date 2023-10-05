ec184c883b27ff2376ee6d8c49f673c199ebe1a1
ww <wwnoov@gmail.com>
2023-10-05 17:43:15 +0900

    Java 공부Array & If

---
+++ CharAtEx.java
@@ -0,0 +1,20 @@
+package com.myStudy.Char;
+/**
+ * 배열 원하는 문자 읽기
+ * */
+public class CharAtEx {
+    public static void main(String[] args) {
+        String ssn ="956241230123";
+        char sex = ssn.charAt(6);
+        switch (sex){
+            case '1':
+            case '3':
+                System.out.println("남자입니다");
+                break;
+            case '2':
+            case '4':
+                System.out.println("여자입니다");
+                break;
+        }
+    }
+}
---
+++ CharGrade.java
@@ -0,0 +1,27 @@
+package com.myStudy.Char;
+/**
+ if문으로 학점 구하는 프로그램
+ * */
+import java.util.Scanner;
+
+public class CharGrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("학점을 입력하세요:");
+        String strX = scanner.nextLine();
+        int score = Integer.parseInt(strX);
+        char grade;
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70){
+            grade = 'C';
+        } else if (score >= 60){
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println(score + "점은" + grade + " 등급입니다");
+    }
+}
