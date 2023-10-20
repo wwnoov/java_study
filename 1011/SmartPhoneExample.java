e6d59793dacc3fd9397559a7562e93ee48d223f8
ww <wwnoov@gmail.com>
2023-10-20 13:40:35 +0900

    Java 상속

--- 1011/SmartPhoneExample.java
+++ 1011/SmartPhoneExample.java
@@ -1,5 +1,7 @@
 package com.myStudy.Inheri;
-
+/**
+ * 생성자 연습
+ * */
 public class SmartPhoneExample {
     public static void main(String[] args) {
         SmartPhone myPhone = new SmartPhone("아이폰","검정");
